d8ce56feada64d287306d7f439ec12a42acda0d6
zhangyubaka <zhangyubaka@gmail.com>
2016-05-05 18:53:53 +0800

    Make it more complex (((

--- bot.py
+++ bot.py
@@ -15,18 +15,23 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-userid = str(input("Please input id who you want fav attack\n"))
-count = input("input number you want to fav!\n")
+def getdata():
+    userid = str(input("Please input id who you want fav attack\n"))
+    count = input("input number you want to fav!\n")
+    fav = api.user_timeline(id = userid, count = count)
 
-fav = api.user_timeline(id = userid, count = count)
+def main():
+    getdata()
+    try:
+        for status in fav:
+            api.create_favorite(status.id_str)
+    except tweepy.error.TweepError as e:
+        if e.args[0][0]['code'] == 139:
+            print("You have already favorited this status! \n")
+        else:
+            print(e.reason)
+    finally:
+            print("Done!")
 
-try:
-    for status in fav:
-        api.create_favorite(status.id_str)
-except tweepy.error.TweepError as e:
-    if e.args[0][0]['code'] == 139:
-        print("You have already favorited this status! \n")
-    else:
-        print(e.reason)
-finally:
-    print("Done!")
+if __name__ == "__main__":
+    main()
