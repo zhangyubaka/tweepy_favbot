ffb33b2265f6125dc9f8d89cb84008aa3f9e76d0
zhangyubaka <zhangyubaka@gmail.com>
2016-05-05 19:11:37 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,25 +4,31 @@
 
 import tweepy
 
+
 consumer_key = ""
 consumer_secret = ""
+
 
 access_token = ""
 access_token_secret = ""
 
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
+
 api = tweepy.API(auth)
+
 
 def tdata():
     userid = str(input("Please input id who you want fav attack\n"))
     count = input("input number you want to fav!\n")
     return userid, count
 
+
 def main():
     t = tdata()
-    fav = api.user_timeline(id = t[0], count = t[1])
+    fav = api.user_timeline(id=t[0], count=t[1])
     try:
         for status in fav:
             api.create_favorite(status.id_str)
@@ -36,3 +42,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
